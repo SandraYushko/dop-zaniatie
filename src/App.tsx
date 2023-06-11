6e2687ebc235c846bed69f55718400322e92db98
SandraYushko <shyria6@gmail.com>
2023-06-12 00:07:42 +0300

    dop zaniatie lesson 01 1/2

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,15 @@ import React, {useState} from 'react';
 import './App.css';
 import {Country} from "./Country";
 
-export type BanknotsType = '' // создадим типы для banknotes -он может быть 'Dollars', 'RUBLS' или 'All'
+export type BanknotsType = 'Dollars' | 'RUBLS' | 'All'
+
 export type MoneyType = {
     banknotes: BanknotsType
-    value: any// не ленимся, убираем заглушку, и пишем правильный тип)
-    number: any// ложку за Димыча, за...
+    value: number
+    number: string
 }
 
-let defaultMoney: any = [  // типизируем
+let defaultMoney: MoneyType[] = [
     {banknotes: 'Dollars', value: 100, number: ' a1234567890'},
     {banknotes: 'Dollars', value: 50, number: ' z1234567890'},
     {banknotes: 'RUBLS', value: 100, number: ' w1234567890'},
@@ -21,19 +22,15 @@ let defaultMoney: any = [  // типизируем
 ]
 
 // типизируем на входе и выходе
-export const moneyFilter = (money: any, filter: any): any => {
-    //если пришел filter со значением 'All', то возвращаем все банкноты
-    //return money.filter... ну да, придется фильтровать
+export const moneyFilter = (money: MoneyType[], filter: BanknotsType): MoneyType[] => {
+    if (filter === "All") return defaultMoney
+    return money.filter(el => el.banknotes === filter)
 }
 
 function App() {
-    // убираем заглушки в типизации и вставляем в качестве инициализационного значения defaultMoney
-    const [money, setMoney] = useState<any>([])
-    const [filterValue, setFilterValue] = useState<any>('')   // по умолчанию указываем все банкноты
-
-    // а вот сейчас притормаживаем. И вдумчиво: константа filteredMoney получает результат функции moneyFilter
-    // в функцию передаем деньги и фильтр, по которому ихбудем выдавать(ретёрнуть)
-    const filteredMoney = moneyFilter(грошы, фильтръ)
+    const [money, setMoney] = useState<MoneyType[]>(defaultMoney)
+    const [filterValue, setFilterValue] = useState<BanknotsType>('All')
+    const filteredMoney = moneyFilter(money, filterValue)
     return (
         <div className="App">
             <Country
